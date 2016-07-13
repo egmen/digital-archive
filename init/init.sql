b518772956835ce57f0285395f6b34ea3c60eeca
egmen <egmen@mail.ru>
2016-07-13 20:30:23 +0300

    [+] Таблица с разрешениями.

--- init/init.sql
+++ init/init.sql
@@ -12,3 +12,23 @@ CREATE TABLE files (
   Size integer,
   FilePath character varying(1024)
 );
+
+-- Создание таблицы с типами разрешений
+CREATE SEQUENCE bittypes MINVALUE 0
+;
+CREATE TABLE "permissionTypes" (
+  Id int PRIMARY KEY DEFAULT 2 ^ nextval('bittypes'),
+  Name character varying(50),
+  VarName character varying(50),
+  BitId varbit DEFAULT CAST(2 ^ currval('bittypes') AS int)::bit(16),
+  n int DEFAULT currval('bittypes')
+);
+
+INSERT INTO "permissionTypes" (Name, VarName)
+VALUES ('Чтение', 'P_READ')
+,('Создание', 'P_CREATE')
+,('Переименование', 'P_RENAME')
+,('Копирование', 'P_COPY')
+,('Перемещение', 'P_MOVE')
+,('Удаление', 'P_DELETE')
+;
