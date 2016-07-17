20d0dffadd29bed14c379e5b1b9eb54942067a22
egmen <egmen@mail.ru>
2016-07-17 22:56:04 +0300

    [+] На всякий случай, вдруг появился NULL.

--- app/togglePermission.sql
+++ app/togglePermission.sql
@@ -1,6 +1,6 @@
 -- Переключение разрешения путём XOR к текущему разрешению
 UPDATE "permissions"
-SET "Permission" = "Permission" # $2
+SET "Permission" = coalesce("Permission", 0) # $2
 WHERE 1 = 1
 	AND "Id" = $1
 	AND "Name" = $3
